4004406d7d033ddfb762fbd4eecc001b023fc5af
mkr0205 <mkr0205@outlook.com>
2024-09-03 05:48:39 +0200

    script

---
+++ input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your username: "
+read -s USERNAME # taks into USERNAME variable
+
+echo "username enterd is : $USERNAME"
+
+read -s PASSWORD
+
+echo "Password entered is : $PASSWORD"
